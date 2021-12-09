04a626a0f9ff3bf5cc09b80b976ab854687b4826
Imad-yes <73041803+Imad-yes@users.noreply.github.com>
2021-12-09 10:05:07 +0100

    Add files via upload

---
+++ coord.py
@@ -0,0 +1,29 @@
+from fltk import *
+
+cree_fenetre(400, 400)
+
+ax = 10
+ay = 10
+bx = 100
+by = 200
+rect1 = (rectangle(ax, ay, bx, by))
+
+
+while True:
+    ev = donne_ev()
+    tev = type_ev(ev)
+
+    # Action dépendant du type d'événement reçu :
+
+    if tev == "ClicGauche":
+        print("Clic gauche au point", (abscisse(ev), ordonnee(ev)))
+        if (ax < abscisse(ev) < bx) and (ay < ordonnee(ev) < by) :
+            efface (rect1) 
+    elif tev == 'Quiite':
+        break
+    else:  # dans les autres cas, on ne fait rien
+        pass
+
+    mise_a_jour()
+
+ferme_fenetre()
