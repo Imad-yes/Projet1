406415ce51deb7eabee9b0132b4562e6cd0f377a
Imad-yes <73041803+Imad-yes@users.noreply.github.com>
2021-12-10 22:18:47 +0100

    Update coord.py

--- coord.py
+++ coord.py
@@ -6,22 +6,26 @@ ax = 10
 ay = 10
 bx = 100
 by = 200
-rect1 = (rectangle(ax, ay, bx, by))
+rectangle(ax, ay, bx, by, tag='allu1')
 
 
 while True:
     ev = donne_ev()
     tev = type_ev(ev)
 
-    if tev == "ClicGauche":
-        print("Clic gauche au point", (abscisse(ev), ordonnee(ev)))
-        if (ax < abscisse(ev) < bx) and (ay < ordonnee(ev) < by) :
-            efface (rect1) 
-    elif tev == 'Quitte':
+    def efface_allu(allu):
+        if tev == "ClicGauche":
+            print('Clic gauche au point', (abscisse(ev), ordonnee(ev)))
+            if (ax < abscisse(ev) < bx) and (ay < ordonnee(ev) < by):
+                efface(allu)
+        else:
+            pass
+        mise_a_jour()
+    efface_allu('allu1')
+    if tev == 'Quitte':
         break
-    else:  
-        pass
 
-    mise_a_jour()
+ferme_fenetre()    
 
-ferme_fenetre()
+
+    
